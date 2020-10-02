1d3cb7db9e92f4bf32e78a13710ce5a17dcdcb1d
Vitaly Lipatov <lav@etersoft.ru>
2020-10-02 14:05:40 +0300

    add missed uninstall_jacarta.sh

---
+++ uninstall_jacarta.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+# TODO: only if not root
+SUDO=sudo
+
+BIARCH=''
+[ "$(distro_info -a)" = "x86_64" ] && BIARCH="i586-"
+
+
+fatal()
+{
+    echo "FATAL: $*" >&2
+    exit 1
+}
+
+
+INSTALL32=''
+INSTALL64=''
+case "$1" in
+    32)
+        INSTALL32="$1"
+        ;;
+    64)
+        INSTALL64="$1"
+        ;;
+    both)
+        INSTALL32="$1"
+        INSTALL64="$1"
+        ;;
+    *)
+        fatal "Run with 32|64|both param"
+esac
+
+
+
+if [ -n "$INSTALL64" ] ; then
+    epme jcPKCS11-2
+fi
+
+if [ -n "$INSTALL32" ] ; then
+    epme ${BIARCH}jcPKCS11-2
+fi
